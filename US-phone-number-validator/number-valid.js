7525278bfb8b6322e17400d0dbe321f2ba8136a5
Katarina Draganov Csordás <155435114+KatarinaDraganovCsordas@users.noreply.github.com>
2024-03-20 14:43:07 +0100

    number-valid.js

---
+++ US-phone-number-validator/number-valid.js
@@ -0,0 +1,32 @@
+
+
+const checkBtn = document.getElementById("check-btn");
+const clearBtn = document.getElementById("clear-btn");
+const userInput = document.getElementById("user-input");
+const resultsDiv = document.getElementById("results-div");
+
+
+checkBtn.addEventListener("click", () => validatePhoneNumber());
+clearBtn.addEventListener("click", () => clearResults());
+
+
+const validatePhoneNumber = () => {
+  const userInput = document.getElementById("user-input").value;
+  const resultsDiv = document.getElementById("results-div");
+
+    var phoneRegex = /^(1\s?)?(\(\d{3}\)|\d{3})([-.\s]?)\d{3}([-.\s]?)\d{4}$/;
+
+    if (userInput === "") {
+        alert("Please provide a phone number.");
+    } else if (phoneRegex.test(userInput)) {
+        resultsDiv.innerText = "Valid US number: " + userInput;
+    } else {
+        resultsDiv.innerText = "Invalid US number: " + userInput;
+    }
+}
+
+
+const clearResults = () => {
+    userInput.value = "";
+    resultsDiv.innerText = "";
+}
