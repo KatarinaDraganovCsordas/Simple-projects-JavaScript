3fff31d4b9dfb0104bbb53028f2a1cfc47f7ab9c
Katarina Draganov Csordás <155435114+KatarinaDraganovCsordas@users.noreply.github.com>
2024-03-28 19:02:31 +0100

    spreadsheet.js

--- Spreadsheet/spreadsheet.js
+++ Spreadsheet/spreadsheet.js
@@ -1,3 +1,18 @@
+const infixToFunction = {
+  "+": (x, y) => x + y,
+  "-": (x, y) => x - y,
+  "*": (x, y) => x * y,
+  "/": (x, y) => x / y,
+}
+
+const infixEval = (str, regex) => str.replace(regex, (_match, arg1, operator, arg2) => infixToFunction[operator](parseFloat(arg1), parseFloat(arg2)));
+
+const highPrecedence = str => {
+  const regex = /([\d.]+)([*\/])([\d.]+)/;
+  const str2 = infixEval(str, regex);
+  return str === str2 ? str : highPrecedence(str2);
+}
+
 const isEven = num => num % 2 === 0;
 const sum = nums => nums.reduce((acc, el) => acc + el, 0);
 const average = nums => sum(nums) / nums.length;
@@ -9,57 +24,4 @@ const median = nums => {
   return isEven(length)
     ? average([sorted[middle], sorted[middle + 1]])
     : sorted[Math.ceil(middle)];
-}
-
-const spreadsheetFunctions = {
-  sum,
-  average,
-  median
-}
-
-const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
-const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map(code => String.fromCharCode(code));
-
-const evalFormula = (x, cells) => {
-  const idToText = id => cells.find(cell => cell.id === id).value;
-  const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
-  const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
-  const elemValue = num => {
-    const inner = character => {
-      return idToText(character + num);
-    }
-    return inner;
-  }
-
-}
-
-window.onload = () => {
-  const container = document.getElementById("container");
-  const createLabel = (name) => {
-    const label = document.createElement("div");
-    label.className = "label";
-    label.textContent = name;
-    container.appendChild(label);
-  }
-  const letters = charRange("A", "J");
-  letters.forEach(createLabel);
-  range(1, 99).forEach(number => {
-    createLabel(number);
-    letters.forEach(letter => {
-      const input = document.createElement("input");
-      input.type = "text";
-      input.id = letter + number;
-      input.ariaLabel = letter + number;
-      input.onchange = update;
-      container.appendChild(input);
-    })
-  })
-}
-
-const update = event => {
-  const element = event.target;
-  const value = element.value.replace(/\s/g, "");
-  if (!value.includes(element.id) && value.startsWith('=')) {
-
-  }
 }
