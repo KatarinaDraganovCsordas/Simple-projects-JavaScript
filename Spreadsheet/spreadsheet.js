dbec98c4103eb49ec6e014c064455f5392460203
Katarina Draganov Csordás <155435114+KatarinaDraganovCsordas@users.noreply.github.com>
2024-03-28 19:04:36 +0100

    spreadsheet.js

--- Spreadsheet/spreadsheet.js
+++ Spreadsheet/spreadsheet.js
@@ -68,3 +68,26 @@ const evalFormula = (x, cells) => {
   const functionExpanded = applyFunction(cellExpanded);
   return functionExpanded === x ? functionExpanded : evalFormula(functionExpanded, cells);
 }
+
+window.onload = () => {
+  const container = document.getElementById("container");
+  const createLabel = (name) => {
+    const label = document.createElement("div");
+    label.className = "label";
+    label.textContent = name;
+    container.appendChild(label);
+  }
+  const letters = charRange("A", "J");
+  letters.forEach(createLabel);
+  range(1, 99).forEach(number => {
+    createLabel(number);
+    letters.forEach(letter => {
+      const input = document.createElement("input");
+      input.type = "text";
+      input.id = letter + number;
+      input.ariaLabel = letter + number;
+      input.onchange = update;
+      container.appendChild(input);
+    })
+  })
+}
