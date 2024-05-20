403097e8fb56740400584c5ed5f666522b64b8f6
Katarina Draganov Csordás <155435114+KatarinaDraganovCsordas@users.noreply.github.com>
2024-05-20 10:44:09 +0200

    color.js

--- Color/color.js
+++ Color/color.js
@@ -5,4 +5,10 @@ const color = document.querySelector(".color");
 btn.addEventListener('click', function() {
   const randomNumber = getRandomNumber();
 
-  
+  color.textContent = colors[randomNumber];
+  document.body.style.backgroundColor = colors[randomNumber];
+})
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
