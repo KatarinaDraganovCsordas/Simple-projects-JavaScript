0a2c2cd2a42643465d51f6c56a631376e02a477f
Katarina Draganov Csordás <155435114+KatarinaDraganovCsordas@users.noreply.github.com>
2024-06-23 12:08:37 +0200

    drag-drop.js

--- Drag and drop/drag-drop.js
+++ Drag and drop/drag-drop.js
@@ -6,7 +6,13 @@
         list.addEventListener("dragstart", function(e){
           let selected = e.target;
 
+          rightBox.addEventListener("dragover", function(e){
+            e.preventDefault();
+          });
 
-          
+          rightBox.addEventListener("drop", function(e){
+            rightBox.appendChild(selected);
+            selected = null;
+          });
         })
       }
