1658ed27a701625581c0fc283e733015735c0dd1
Katarina Draganov Csordás <155435114+KatarinaDraganovCsordas@users.noreply.github.com>
2024-06-27 11:01:31 +0200

    email.js

---
+++ Email/email.js
@@ -0,0 +1,4 @@
+      const scriptURL = 'https://script.google.com/macros/s/AKfycbwwjhLDe7fqvb5uloyfV53Jzmu4bWj_OCukCl7mbWJF7W5VSNl0xXUGA5Iu6_zZGeGj/exec'
+      const form = document.forms['submit-to-google-sheet']
+      const msg = document.getElementById("msg");
+   
