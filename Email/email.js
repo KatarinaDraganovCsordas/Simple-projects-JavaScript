897e3f80635a3cea91d0e27efb040f3246e6e06a
Katarina Draganov Csordás <155435114+KatarinaDraganovCsordas@users.noreply.github.com>
2024-06-27 11:01:56 +0200

    email.js

--- Email/email.js
+++ Email/email.js
@@ -1,4 +1,16 @@
       const scriptURL = 'https://script.google.com/macros/s/AKfycbwwjhLDe7fqvb5uloyfV53Jzmu4bWj_OCukCl7mbWJF7W5VSNl0xXUGA5Iu6_zZGeGj/exec'
       const form = document.forms['submit-to-google-sheet']
       const msg = document.getElementById("msg");
-   
+    
+      form.addEventListener('submit', e => {
+        e.preventDefault()
+        fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+          .then(response => {
+            msg.innerHTML = "Thank you for subscribing!"
+            setTimeout(() => {
+              msg.innerHTML = ""
+            }, 5000);
+            form.reset()
+          })
+          .catch(error => console.error('Error!', error.message))
+      })
