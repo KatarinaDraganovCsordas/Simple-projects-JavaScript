e0563df17b9261ffab6ce1ab666e37323a8aa269
Katarina Draganov Csordás <155435114+KatarinaDraganovCsordas@users.noreply.github.com>
2024-07-01 10:34:56 +0200

    gallery.js

---
+++ Gallery/gallery.js
@@ -0,0 +1,5 @@
+      let scrollContainer = document.querySelector(".gallery");
+      let backBtn = document.getElementById("backBtn");
+      let nextBtn = document.getElementById("nextBtn");
+
+      
