b786e50be5dd5765950b19c5bcfb65e9bc44e3d5
Katarina Draganov Csordás <155435114+KatarinaDraganovCsordas@users.noreply.github.com>
2024-07-07 20:23:31 +0200

    meme.js

--- Meme/meme.js
+++ Meme/meme.js
@@ -9,3 +9,14 @@ const updateDetails = (url, title, author) => {
   memeAuthor.innerHTML = `Meme by: ${author}`;
 };
 
+const generateMeme = () => {
+  fetch("https://meme-api.com/gimme/wholesomememes")
+    .then((response) => response.json())
+    .then((data) => {
+      updateDetails(data.url, data.title, data.author);
+    });
+};
+
+generateMemeBtn.addEventListener("click", generateMeme);
+
+generateMeme();
