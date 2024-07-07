a1a0088a6ccd5ad3afdcc76f7057587e418041fe
Katarina Draganov Csordás <155435114+KatarinaDraganovCsordas@users.noreply.github.com>
2024-07-07 20:22:09 +0200

    meme.js

---
+++ Meme/meme.js
@@ -0,0 +1,4 @@
+const generateMemeBtn = document.querySelector(".meme-generator .generate-meme-btn");
+const memeImage = document.querySelector(".meme-generator img");
+const memeTitle = document.querySelector(".meme-generator .meme-title");
+const memeAuthor = document.querySelector(".meme-generator .meme-author");
