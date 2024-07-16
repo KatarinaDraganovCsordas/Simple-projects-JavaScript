b6798e20ddaf315f7a76b1ce183ddec8fead4d35
Katarina Draganov Csordás <155435114+KatarinaDraganovCsordas@users.noreply.github.com>
2024-07-16 19:54:57 +0200

    notes.js

--- Notes/notes.js
+++ Notes/notes.js
@@ -9,4 +9,20 @@ function showNotes() {
 showNotes();
 
 
+function updateStorage() {
+  localStorage.setItem("notesss", notesContainer.innerHTML);
+}
+
+
+
+createBtn.addEventListener("click", ()=>{
+  let inputBox = document.createElement("p");
+  let img = document.createElement("img");
+
+  inputBox.className = "input-box";
+  inputBox.setAttribute("contenteditable", "true");
+  img.src = "kepek/delete.png";
+  notesContainer.appendChild(inputBox).appendChild(img);
+})
+
 
