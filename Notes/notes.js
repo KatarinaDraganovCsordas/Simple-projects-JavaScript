00d6aa4efe7c0cdd75a239ce9b8608242bc3eb6c
Katarina Draganov Csordás <155435114+KatarinaDraganovCsordas@users.noreply.github.com>
2024-07-16 19:54:03 +0200

    notes.js

---
+++ Notes/notes.js
@@ -0,0 +1,12 @@
+const notesContainer = document.querySelector(".notes-container");
+const createBtn = document.querySelector(".btn");
+let notes = document.querySelectorAll(".input-box");
+
+
+function showNotes() {
+  notesContainer.innerHTML = localStorage.getItem("notesss");
+}
+showNotes();
+
+
+
