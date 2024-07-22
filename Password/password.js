1d6099411e870c114c7af61a8f3801350fabf5d9
Katarina Draganov Csordás <155435114+KatarinaDraganovCsordas@users.noreply.github.com>
2024-07-22 11:49:06 +0200

    password.js

--- Password/password.js
+++ Password/password.js
@@ -3,6 +3,12 @@ var pass = document.getElementById("password");
       var str = document.getElementById("strength");
 
       pass.addEventListener('input', ()=>{
-        
+        if(pass.value.length > 0) {
+          msg.style.display = "block";
+        } else {
+          msg.style.display = "none";
+        }
 
+
+       
       })
