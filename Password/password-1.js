95d530eb902245ba1635c065afdec11d1357fc74
Katarina Draganov Csordás <155435114+KatarinaDraganovCsordas@users.noreply.github.com>
2024-07-25 10:47:09 +0200

    password-1.js

--- Password/password-1.js
+++ Password/password-1.js
@@ -1,4 +1,13 @@
  let eyeicon = document.getElementById("eyeicon");
-      let password = document.getElementById("password");
+ let password = document.getElementById("password");
 
-      
+      eyeicon.onclick = function() {
+        if(password.type === "password") {
+          password.type = "text";
+          eyeicon.src = "kepek/eye-open.png"
+        } else {
+          password.type = "password"
+          eyeicon.src = "kepek/eye-close.png"
+
+        }
+      }
