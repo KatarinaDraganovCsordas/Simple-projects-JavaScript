23df7430a76c5811459fa7afc4e0a9cc693d11aa
Katarina Draganov Csordás <155435114+KatarinaDraganovCsordas@users.noreply.github.com>
2024-08-14 20:02:14 +0200

    product.js

--- Product/product.js
+++ Product/product.js
@@ -7,17 +7,15 @@ btn[0].onclick = function() {
     bt.classList.remove("active");
   }
   this.classList.add("active");
+}
+
+btn[1].onclick = function() {
+  productImg.src = "kepek/product-img/image2.png";
+  for(bt of btn) {
+    bt.classList.remove("active");
   }
-
-
-      btn[1].onclick = function() {
-        productImg.src = "kepek/product-img/image2.png";
-
-        for(bt of btn) {
-          bt.classList.remove("active");
-        }
-        this.classList.add("active");
-      }
+  this.classList.add("active");
+}
 
 
       btn[2].onclick = function() {
