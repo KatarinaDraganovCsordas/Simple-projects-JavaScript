c6a369c333b9d9888cfb68b866ec92bce4dd0381
Katarina Draganov Csordás <155435114+KatarinaDraganovCsordas@users.noreply.github.com>
2024-10-02 18:47:47 +0200

    quiz.js

--- Quiz/quiz.js
+++ Quiz/quiz.js
@@ -1,9 +1,35 @@
-
-function handleNextButton() {
-  currentQuestionIndex++;
-  if(currentQuestionIndex < questions.length) {
-    showQuestion();
-  } else {
-    showScore();
+const questions = [
+  {
+    question: "Which is the largest animal in the world?",
+    answers: [
+      {text: "Shark", correct: false},
+      {text: "Blue whale", correct: true},
+      {text: "Elephant", correct: false},
+      {text: "Giraffe", correct: false},
+    ]
+  }, {
+    question: "Which is the largest desert in the world?",
+    answers: [
+      {text: "Gobi", correct: false},
+      {text: "Sahara", correct: false},
+      {text: "Antarctica", correct: true},
+      {text: "Kalahari", correct: false},
+    ]
+  }, {
+    question: "Which is the smallest contry in the world?",
+    answers: [
+      {text: "Vatican City", correct: true},
+      {text: "Bhutan", correct: false},
+      {text: "Nepal", correct: false},
+      {text: "Luxenburg", correct: false},
+    ]
+  }, {
+    question: "Which is the smallest continent in the world?",
+    answers: [
+      {text: "Asia", correct: false},
+      {text: "Australia", correct: true},
+      {text: "Africa", correct: false},
+      {text: "Arctic", correct: false},
+    ]
   }
-}
+];
