46e445b080249bb38a4b876b89b965279ba2dd00
Katarina Draganov Csordás <155435114+KatarinaDraganovCsordas@users.noreply.github.com>
2024-10-03 20:19:32 +0200

    quiz.js

--- Quiz/quiz.js
+++ Quiz/quiz.js
@@ -38,3 +38,5 @@ const questionElement = document.getElementById("question");
 const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
 
+let currentQuestionIndex = 0;
+let score = 0;
