860e4cb08e745e69142093c0da5f9be456461645
Katarina Draganov Csordás <155435114+KatarinaDraganovCsordas@users.noreply.github.com>
2024-10-09 18:37:04 +0200

    quiz.js

--- Quiz/quiz.js
+++ Quiz/quiz.js
@@ -74,3 +74,24 @@ function resetState() {
     answerButtons.removeChild(answerButtons.firstChild);
   }
 }
+
+function selectAnswer(e) {
+  const selectedBtn = e.target;
+  const isCorrect = selectedBtn.dataset.correct === "true";
+
+  if(isCorrect) {
+    selectedBtn.classList.add("correct");
+    score++;
+  } else {
+    selectedBtn.classList.add("incorrect");
+  }
+
+
+  Array.from(answerButtons.children).forEach(button => {
+    if(button.dataset.correct === "true") {
+      button.classList.add("correct");
+    }
+    button.disabled = true;
+  });
+  nextButton.style.display = "block";
+}
